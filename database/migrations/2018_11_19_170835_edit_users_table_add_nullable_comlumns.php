4300279a7faa0bd20f6412983720ecc554c33790
il-kow@mail.ru <il-kow@mail.ru>
2018-11-19 22:24:34 +0400

    Added migration for claims nullable

---
+++ database/migrations/2018_11_19_170835_edit_users_table_add_nullable_comlumns.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditUsersTableAddNullableComlumns extends Migration
+{
+    /**
+     * EditClaimsTableMakeEmailNullable constructor.
+     * Возможность оставить тип enum в БД
+     */
+    public function __construct()
+    {
+        DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('claims', function (Blueprint $table) {
+            $table->string('firstname')->nullable()->change();
+            $table->string('middlename')->nullable()->change();
+            $table->string('lastname')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('claims', function (Blueprint $table) {
+            $table->string('firstname')->nullable()->change();
+            $table->string('middlename')->nullable()->change();
+            $table->string('lastname')->nullable()->change();
+        });
+    }
+}
